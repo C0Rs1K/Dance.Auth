386bb20ca77190941fe93b47eb5c48a7315ee6fd
Егор Толоконников <90565476+C0Rs1K@users.noreply.github.com>
2024-09-16 12:58:51 +0300

    Create data context and configuration

--- Dance.Auth.Data/Context/DanceAuthContext.cs
+++ Dance.Auth.Data/Context/DanceAuthContext.cs
@@ -1,6 +1,18 @@
-﻿namespace Dance.Auth.Data.Context;
+﻿using Dance.Auth.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
-public class DanceAuthContext
+namespace Dance.Auth.Data.Context;
+
+public class DanceAuthContext(DbContextOptions<DanceAuthContext> options)
+    : IdentityDbContext<User, IdentityRole<Guid>, Guid>(options)
 {
-    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
 }
