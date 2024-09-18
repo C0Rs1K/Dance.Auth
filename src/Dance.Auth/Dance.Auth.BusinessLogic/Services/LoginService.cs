4ce46b7dc98c7ee2b0077f4b755c652370392593
Egor Tolokonnikov <90565476+C0Rs1K@users.noreply.github.com>
2024-09-18 16:38:26 +0300

    Create login and registration services

--- src/Dance.Auth/Dance.Auth.BusinessLogic/Services/LoginService.cs
+++ src/Dance.Auth/Dance.Auth.BusinessLogic/Services/LoginService.cs
@@ -1,6 +1,41 @@
-﻿namespace Dance.Auth.Business.Services;
+﻿using Dance.Auth.Data.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Authentication.BearerToken;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Http.Metadata;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
-public class LoginService
+namespace Dance.Auth.Business.Services;
+
+public class LoginService(SignInManager<User> signInManager) : ILoginService
 {
-    
+    public Task Login(LoginRequestDto loginRequest, bool? useCookies, bool? useSessionCookies)
+    {
+        var useCookieScheme = (useCookies == true) || (useSessionCookies == true);
+        var isPersistent = (useCookies == true) && (useSessionCookies != true);
+        signInManager.AuthenticationScheme = useCookieScheme ? IdentityConstants.ApplicationScheme : IdentityConstants.BearerScheme;
+
+        var result = await signInManager.PasswordSignInAsync(loginRequest.Email, loginRequest.Password, isPersistent, lockoutOnFailure: true);
+
+        if (!result.Succeeded)
+        {
+            throw new UnauthorizedException("Incorrect username or password");
+        }
+    }
+
+    public Task Logout()
+    {
+        await signInManager.SignOutAsync();
+    }
 }
