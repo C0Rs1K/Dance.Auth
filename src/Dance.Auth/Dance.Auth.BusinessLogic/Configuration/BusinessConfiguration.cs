188c69d036b540ce1b851e6f049d113245322290
Egor Tolokonnikov <90565476+C0Rs1K@users.noreply.github.com>
2024-09-19 18:17:12 +0300

    Add fluentValidation and auto validation configure

--- src/Dance.Auth/Dance.Auth.BusinessLogic/Configuration/BusinessConfiguration.cs
+++ src/Dance.Auth/Dance.Auth.BusinessLogic/Configuration/BusinessConfiguration.cs
@@ -2,5 +2,10 @@
 
 public class BusinessConfiguration
 {
-    
+    public static IServiceCollection ConfigureAutoFluentValidation(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssembly(typeof(LoginRequestValidator).Assembly);
+        services.AddFluentValidationAutoValidation(); 
+        return services; 
+    }
 }
---
+++ src/Dance.Auth/Dance.Auth.BusinessLogic/Validators/LoginRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Dance.Auth.Business.Validators;
+
+public class LoginRequestValidator : AbstractValidator<LoginRequestDto>
+{
+    public LoginRequestValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("A valid email is required");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required");
+    }
+}
