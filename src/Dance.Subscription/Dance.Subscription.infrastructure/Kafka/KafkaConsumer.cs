d27827122a9f7d852943c9abd251a401fb87c5de
Егор Толоконников <90565476+C0Rs1K@users.noreply.github.com>
2024-10-21 17:16:57 +0300

    create subscription consumer

---
+++ src/Dance.Subscription/Dance.Subscription.infrastructure/Kafka/KafkaConsumer.cs
@@ -0,0 +1,31 @@
+﻿using Confluent.Kafka;
+using Dance.Subscription.Domain.Entities;
+using MediatR;
+using Microsoft.Extensions.Hosting;
+
+namespace Dance.Subscription.infrastructure.Kafka;
+
+public class KafkaConsumer(ISender sender, IConsumer<string, Student> consumer) : BackgroundService
+{
+    private const string Topic = "registrations";
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        try
+        {
+            consumer.Subscribe(Topic);
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var result = consumer.Consume(stoppingToken);
+
+                var student = result.Message.Value;
+                var command = new ProcessStudentRegistrationCommand(student);
+                await sender.Send(command, stoppingToken);
+            }
+        }
+        finally
+        {
+            consumer.Close();
+        }
+    }
+}
