1a515dcb81c170926f54164bf0d1636aa90bdfe6
Raman Shreitar <shreiter@topcase.ru>
2024-12-17 22:19:04 +0300

    f

--- src/Dance.Auth/Dance.Auth.Api/Program.cs
+++ src/Dance.Auth/Dance.Auth.Api/Program.cs
@@ -3,7 +3,7 @@ using Dance.Auth.Api.Configuration;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.ConfigureWebApplicationBuilder();
-
+builder.WebHost.UseUrls("http://*:50001");
 var app = builder.Build();
 
 app.ConfigureApplicationPipeline();
--- src/Dance.Store/Dance.Store.Api/Program.cs
+++ src/Dance.Store/Dance.Store.Api/Program.cs
@@ -3,7 +3,7 @@ using Dance.Store.Api.Configuration;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.ConfigureApplicationBuilder();
-
+builder.WebHost.UseUrls("http://*:50002");
 var app = builder.Build();
 
 app.ConfigureApplication();
